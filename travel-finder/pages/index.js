4bb68aedff8cd22229803b34c7d425c74cfb2b63
amiragucher <amiragucher@gmail.com>
2023-02-28 16:55:00 +0000

    Footer displaying

--- travel-finder/pages/index.js
+++ travel-finder/pages/index.js
@@ -1,12 +1,14 @@
 import NavBar from './Components/NavBar/NavBar'
 import Style from "../styles/Home.module.css"
 import About from "./Components/About/About.jsx"
+import Footer from "./Components/Footer/Footer.jsx"
 
 export default function Home() {
   return (
     <div className={Style.container}>
       <NavBar />
       <About/>
+      <Footer/>
     </div>
   )
 }
