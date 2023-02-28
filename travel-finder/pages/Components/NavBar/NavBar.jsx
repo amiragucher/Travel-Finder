1b810e3cdb47ad0c0ebaa402eb5c5bf43fa76efe
amiragucher <amiragucher@gmail.com>
2023-02-28 16:36:53 +0000

    Font size changed for navbar

--- travel-finder/pages/Components/NavBar/NavBar.jsx
+++ travel-finder/pages/Components/NavBar/NavBar.jsx
@@ -6,8 +6,8 @@ import Style from "../NavBar/NavBar.module.css";
         <div>
             <div className={Style.NavBar}>
             <h1 className={Style.Title}>Travel Finder</h1>
-            <h1 className={Style.Links}>About</h1>
-            <h1 className={Style.Links}>Download Today</h1>
+            <h2 className={Style.Links}>About</h2>
+            <h2 className={Style.Links}>Download Today</h2>
             </div>
         </div>
     );
