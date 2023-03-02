b8d4b0d266798cd2826afb00a4decde92633d383
amiragucher <amiragucher@gmail.com>
2023-03-02 16:08:01 +0000

    Text in the navbar fixed to the right

--- travel-finder/pages/Components/NavBar/NavBar.jsx
+++ travel-finder/pages/Components/NavBar/NavBar.jsx
@@ -6,8 +6,10 @@ import Style from "../NavBar/NavBar.module.css";
         <div>
             <div className={Style.NavBar}>
             <h1 className={Style.Title}>Travel Finder</h1>
+            <div className={Style.LinksContainer}>
             <h2 className={Style.Links}>About</h2>
             <h2 className={Style.Links}>Download Today</h2>
+            </div>
             </div>
         </div>
     );
