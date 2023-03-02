a6658d8a1d2426a73e78c3878281bfa41a2eda70
amiragucher <amiragucher@gmail.com>
2023-03-02 16:32:12 +0000

    Footer contact information added-flex working

--- travel-finder/pages/Components/Footer/Footer.jsx
+++ travel-finder/pages/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import Style from "../Footer/Footer.module.css"
 export default function (){
     return (
         <div className={Style.FooterContainer}>
-        <div className={Style.FooterInfo}>
-            <h1>Travel Finder</h1>
+            <h1 className={Style.FooterTitle}>Travel Finder</h1>
+            <div className={Style.FooterInfo}>
             <h3>Contact</h3>
             <h3>Email: travelfinder@gmail.com</h3>
             <h3>Contact Number: + 7877631245</h3>
